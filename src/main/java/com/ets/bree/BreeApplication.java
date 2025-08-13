94aef5b65dddc6ac8c753623a8027bcf796df44e
Silva Enzo (CaP/ETS2) <ien1ca@bosch.com>
2025-08-13 08:37:20 -0300

    Instalação do projeto para MySQL

---
+++ src/main/java/com/ets/bree/BreeApplication.java
@@ -0,0 +1,13 @@
+package com.ets.bree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ets/bree/BreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ets.bree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
