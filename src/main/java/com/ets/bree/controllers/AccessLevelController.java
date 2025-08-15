716a70094594ddd43e06fb630799dda1fdfb5466
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-08-15 08:35:29 -0300

    AccessLevelController getAccessLevel()

--- src/main/java/com/ets/bree/controllers/AccessLevelController.java
+++ src/main/java/com/ets/bree/controllers/AccessLevelController.java
@@ -1,12 +1,12 @@
 package com.ets.bree.controllers;
-
 import com.ets.bree.models.AccessLevel;
 import com.ets.bree.services.AccessLevelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/access_levels")
@@ -20,8 +20,10 @@ public class AccessLevelController {
         return ResponseEntity.ok(service.getAll());
     }
 
-//    @GetMapping
-//    public String teste(){
-//        return "Hello";
-//    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getAccessLevel(@PathVariable Long id) {
+        Optional<AccessLevel> accessLevel = service.getById(id);
+        if(accessLevel.isPresent()) return ResponseEntity.ok(accessLevel.get());
+        else return ResponseEntity.status(HttpStatus.NOT_FOUND).body("404 Not Found");
+    }
 }
--- src/main/java/com/ets/bree/services/AccessLevelService.java
+++ src/main/java/com/ets/bree/services/AccessLevelService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AccessLevelService {
@@ -15,6 +16,10 @@ public class AccessLevelService {
 
     public List<AccessLevel> getAll() {
         return repository.findAll();
+    }
+
+    public Optional<AccessLevel> getById(Long id) {
+        return repository.findById(id);
     }
 
 }
