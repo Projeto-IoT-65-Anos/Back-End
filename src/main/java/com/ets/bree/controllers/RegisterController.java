815cc0500ee3bbbab879b997d4b3f9f72ed0a291
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-08-24 18:35:06 -0300

    Registers GET

--- src/main/java/com/ets/bree/controllers/RegisterController.java
+++ src/main/java/com/ets/bree/controllers/RegisterController.java
@@ -1,9 +1,14 @@
 package com.ets.bree.controllers;
 
+import com.ets.bree.models.Register;
 import com.ets.bree.services.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/registers")
@@ -12,5 +17,8 @@ public class RegisterController {
     @Autowired
     private RegisterService service;
 
-
+    @GetMapping
+    public ResponseEntity<List<Register>> getAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
 }
--- src/main/java/com/ets/bree/services/RegisterService.java
+++ src/main/java/com/ets/bree/services/RegisterService.java
@@ -1,12 +1,19 @@
 package com.ets.bree.services;
 
+import com.ets.bree.models.Register;
 import com.ets.bree.repositories.RegisterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RegisterService {
 
     @Autowired
     private RegisterRepository repository;
+
+    public List<Register> findAll() {
+        return repository.findAll();
+    }
 }
