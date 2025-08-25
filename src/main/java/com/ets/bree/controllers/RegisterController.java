f5f9cfae356fc5102becf4975015d904aefc176f
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-08-25 01:28:24 -0300

    Register POST

--- src/main/java/com/ets/bree/controllers/RegisterController.java
+++ src/main/java/com/ets/bree/controllers/RegisterController.java
@@ -1,13 +1,12 @@
 package com.ets.bree.controllers;
 
+import com.ets.bree.dtos.RegisterDto;
 import com.ets.bree.models.Register;
 import com.ets.bree.services.RegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +26,13 @@ public class RegisterController {
     public ResponseEntity<Register> getRegister(@PathVariable long id) {
         return service.findById(id)
                 .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Register> postRegister(@RequestBody RegisterDto dto) {
+        return service.post(dto)
+                .map(r -> ResponseEntity.status(HttpStatus.CREATED).body(r))
                 .orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/ets/bree/services/RegisterService.java
+++ src/main/java/com/ets/bree/services/RegisterService.java
@@ -1,7 +1,11 @@
 package com.ets.bree.services;
 
+import com.ets.bree.dtos.RegisterDto;
+import com.ets.bree.models.Device;
 import com.ets.bree.models.Register;
+import com.ets.bree.repositories.DeviceRepository;
 import com.ets.bree.repositories.RegisterRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +17,8 @@ public class RegisterService {
 
     @Autowired
     private RegisterRepository repository;
+    @Autowired
+    private DeviceRepository deviceRepository;
 
     public List<Register> findAll() {
         return repository.findAll();
@@ -20,5 +26,16 @@ public class RegisterService {
 
     public Optional<Register> findById(long id) {
         return repository.findById(id);
+    }
+
+    public Optional<Register> post(RegisterDto dto) {
+        Register register = new Register();
+        BeanUtils.copyProperties(dto, register);
+        Optional<Device> device = deviceRepository.findById(dto.deviceID());
+        return device
+                .map(d -> {
+                    register.setDevice(d);
+                    return repository.save(register);
+                });
     }
 }
